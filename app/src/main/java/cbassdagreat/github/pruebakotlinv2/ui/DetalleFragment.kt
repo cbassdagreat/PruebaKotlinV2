13b2effdfd6b96dd7b9928efeaac665f0caa25ce
Cbass <claveria.sebastian@gmail.com>
2022-04-25 17:56:37 -0400

    layout ready

---
+++ app/src/main/java/cbassdagreat/github/pruebakotlinv2/ui/DetalleFragment.kt
@@ -0,0 +1,20 @@
+package cbassdagreat.github.pruebakotlinv2.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cbassdagreat.github.pruebakotlinv2.R
+
+
+class DetalleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detalle, container, false)
+    }
+
+}
